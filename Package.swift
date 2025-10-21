26155da73e0ac5e0f7fbcca4e54b5e63aa583629
Anthony Crognale <anthony.crognale@liquid.ai>
2025-10-21 16:04:50 -0400

    Release v0.7.1
    - Update binary target URLs for LeapSDK and LeapModelDownloader to v0.7.1
    - Update checksums for both XCFrameworks
    - Update CocoaPods podspec to v0.7.1

--- Package.swift
+++ Package.swift
@@ -27,23 +27,23 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "LeapSDK",
-      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.0/LeapSDK.xcframework.zip", checksum: "0cdb5350c726e460613ebf86fe8ebf9bb742491ec45a4ba00f6b980b5e970495"
+      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.1/LeapSDK.xcframework.zip", checksum: "943d7a1e72cf5b6e8df0ca75d21c60c2347f21f8980b78d41018da032702ec4e"
     ),
     .binaryTarget(
       name: "InferenceEngine",
-      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.0/inference_engine.xcframework.zip", checksum: "39c901daed930ce1d359da2c26c1e9e2fd6984440a730d0b6676077a0f23cd0b"
+      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.1/inference_engine.xcframework.zip", checksum: "d1b36d558fa4ca9c68a159da529a4294f05a37d7687fe823157a330acec1cb4a"
     ),
     .binaryTarget(
       name: "InferenceEngineExecutorchBackend",
-      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.0/inference_engine_executorch_backend.xcframework.zip", checksum: "fc2e8715348223129a98d5186a243da8747cd93c4b249b298d6b73e96d08ed9f"
+      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.1/inference_engine_executorch_backend.xcframework.zip", checksum: "367fbbc9739b0d6ef2b10cb8d958d9488808b9483c48450bcd4402d0fd708e82"
     ),
     .binaryTarget(
       name: "InferenceEngineLlamaCppBackend",
-      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.0/inference_engine_llamacpp_backend.xcframework.zip", checksum: "1800b40ec5d32dbea146c043065a50d7a95c301d9caa64bb4076a195611cdd77"
+      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.1/inference_engine_llamacpp_backend.xcframework.zip", checksum: "5031d7edb28a141d987d65ef935f57644a20200a410d5e1f1a84a9a781859506"
     ),
     .binaryTarget(
       name: "LeapModelDownloader",
-      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.0/LeapModelDownloader.xcframework.zip", checksum: "10a6281465aa53ac7a6f8fd5d4d1fc80dbf9868c0a30142dd1294a229ebe630e"
+      url: "https://github.com/Liquid4All/leap-ios/releases/download/v0.7.1/LeapModelDownloader.xcframework.zip", checksum: "06dbba59fe7f80988ea536fd943ee9e0357c0871aca4d9559a0f85623cacd828"
     ),
     .target(
       name: "LeapSDKSupport",
